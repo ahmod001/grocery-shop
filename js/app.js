e332cbde8574b50e886b5bd9cccab3cdfbd25e8b
ahmod001 <ahmodh08@gmail.com>
2023-03-06 20:10:43 +0600

    navbar, title-section & coustomers-choiceproduct section added

---
+++ js/app.js
@@ -0,0 +1,61 @@
+// Customer Choice product injected from here
+document.getElementById('customer-choice-product').innerHTML = `
+<div class="text-center py-5">
+                <p>Customer's Choice</p>
+                <h3 class="fw-bold">Check Our Products</h3>
+            </div>
+
+            <!-- Product images -->
+
+            <!-- #row-1 -->
+            <div class="row pb-4" data-aos="fade-right">
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="rounded img-fluid"
+                            src="https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg" alt="">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="rounded img-fluid"
+                            src="https://images.pexels.com/photos/33783/olive-oil-salad-dressing-cooking-olive.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="img-fluid rounded"
+                            src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="">
+                    </div>
+                </div>
+            </div>
+
+            <!-- #row-2 -->
+            <div class="row overflow-x-hidden" data-aos="fade-left">
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="rounded img-fluid"
+                            src="https://images.pexels.com/photos/13118857/pexels-photo-13118857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="rounded img-fluid"
+                            src="https://images.pexels.com/photos/1300975/pexels-photo-1300975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="">
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="text-center">
+                        <img class="img-fluid rounded"
+                            src="https://images.pexels.com/photos/12809556/pexels-photo-12809556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="">
+                    </div>
+                </div>
+            </div>`
+
+            
+// For Animation
+AOS.init({ once: true });
+
