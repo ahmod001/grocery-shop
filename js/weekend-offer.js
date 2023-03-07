66b74a5d8a539b21651cb42bf2e416f0d4b82507
ahmod001 <ahmodh08@gmail.com>
2023-03-08 01:20:07 +0600

    product card added

--- js/weekend-offer.js
+++ js/weekend-offer.js
@@ -1,5 +1,5 @@
 // Weekend Offer Section injeced from here
-document.getElementById('weekend-offer').innerHTML =`
+document.getElementById('weekend-offer').innerHTML = `
 <div class="row overflow-x-hidden rounded weekend-offer py-5">
 <!-- #Image -->
 <div class="col-md-5 col-lg-6 d-flex justify-content-center align-items-center pb-4 pb-md-0"
@@ -22,28 +22,28 @@ document.getElementById('weekend-offer').innerHTML =`
                 <div class="row text-dark pt-4">
                     <div class="col-3">
                         <div class="rounded py-1 text-center bg-light">
-                            <h3 class="fw-bold">02</h3>
+                            <h3 class="fw-bold" id="days">00</h3>
                             <h6>Days</h6>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="rounded py-1 text-center bg-light">
-                            <h3 class="fw-bold">17</h3>
+                            <h3 class="fw-bold" id="hours">00</h3>
                             <h6>Hours</h6>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="rounded py-1 text-center bg-light">
-                            <h3 class="fw-bold">47</h3>
+                            <h3 class="fw-bold" id="minutes">00</h3>
                             <h6>Minutes</h6>
                         </div>
                     </div>
 
                     <div class="col-3">
                         <div class="rounded py-1 text-center bg-light">
-                            <h3 class="fw-bold">57</h3>
+                            <h3 class="fw-bold" id="seconds">00</h3>
                             <h6>Seconds</h6>
                         </div>
                     </div>
@@ -54,5 +54,34 @@ document.getElementById('weekend-offer').innerHTML =`
 </div>
 </div>`;
 
+// 7days CountDown Function here
+let countDownDate = new Date("Mar 14, 2023").getTime();
+const setTime = setInterval(() => {
+    // Get Time 
+    const duration = countDownDate - Date.now();
+
+    // this condition will increase old date by 1week
+    if (duration <= 0) {
+        countDownDate += 604800000;
+    }
+
+    const remainingDays = Math.floor(duration / (1000 * 60 * 60 * 24));
+    const remaingHours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const remaingMinutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+    const remaingSeconds = Math.floor((duration % (1000 * 60)) / (1000))
+
+
+    // #DOM Function
+    const DOM = (id, variable) => {
+        const con = variable.toString().length == 1 ? document.getElementById(id).innerText = `0${variable}` : document.getElementById(id).innerText = variable;
+    }
+
+    // DOM
+    DOM('days', remainingDays);
+    DOM('hours', remaingHours);
+    DOM('minutes', remaingMinutes);
+    DOM('seconds', remaingSeconds);
+
+}, 1000)
 // For Animation
 AOS.init({ once: true });
